08085dc6c1d1239f0c073240f0712bf7a26630c8
Jose Duarte <josepipe0909@gmail.com>
2023-02-09 18:53:27 -0500

    añadiendo todas las funciones reales

--- ejercicio1.rkt
+++ ejercicio1.rkt
@@ -1,4 +1,7 @@
 #lang racket/base
+
+;; Ejerciocio #1 Jose Felipe Duarte Coronado Codigo:1003661696
+
 ;; 1- Funcion multiplicación
 ;(* x y z ...) → number
 
@@ -144,11 +147,140 @@
 (complex? (asin 32)) ;#t
 (complex?  (acos (abs (+(* 3 1/2)(/ 2 28))))) ;#t
 
-;; 17 Funcion  que devuelve el conjugado complejo de un número complejo x.
+;; 17- Funcion  que devuelve el conjugado complejo de un número complejo x.
 ; (conjugate x) → number
 
 ;   x : number
-(conjugate -2-5i)
-(conjugate (make-polar 3 4))
+; (conjugate -2-5i) -2+5i
+; (conjugate (make-polar 3 4)) #i-1.960930862590836+2.2704074859237844i
+; (conjugate 2+3) -2-3i
+
+; 18-Funcion coseno
+; (cos x) → number
+;   x : number
+
+;19-Funcion hiperbolica de coseno
+; (cosh x) → number
+;   x : number
+
+;20-Determina el tiempo actual en segundos transcurridos (desde una fecha de inicio específica de la plataforma)
+(current-seconds) ;1675979132
+;; no se pueden proporcionar mas ejemplos :(
+
+;21- funcion que determina el denomidado de un racional
+; (denominator x) → integer
+;   x : rational?
+
+;22- da el numero e
+;e : real
+
+; 23- funcion que determina si el numero es par 
+;(even? x) → boolean?
+ ; x : integer
+
+ ;24- funcion que convierte un numero exacto a uno inexacto
+;  (exact->inexact x) → number
+;   x : number
+
+;25 Funcion que determina si un numero es exacto
+;(exact? x) → boolean?
+;x : number
+
+;26 -Funcion que determina e elevado a un numero
+;(exp x) → number
+;  x : number
+
+; 27- funcion que calcule exponenciales
+;(expt x y) → number
+;   x : number
+;   y : number
+
+;28 -funcion piso, redondea el numero al entero mas pequeño
+; (floor x) → integer
+;   x : real
+
+;29 -Funcion que calcule el maximo comun divisior 
+; (gcd x y ...) → integer
+;   x : integer
+;   y : integer
+
+;30 -Funcion que extrae la parte imaginaria de un numero
+; (imag-part x) → real
+;   x : number
+
+;31 -Funcion que aproxima un numero inexacto a uno exacto
+; (inexact->exact x) → number
+;   x : number
+
+;32 -Funcion que determina si un número es inexacto.
+; (inexact? x) → boolean?
+;   x : number
+
+;33 -Funcion que busca el caracter correspondiente al numero entero exacto dado en la tabla ASCII
+; (integer->char x) → char
+;   x : exact-integer?
+
+;34 -Funcion que calcula la raiz cuadrada y la aproxima al entero o entera-imaginaria de un entero
+; (integer-sqrt x) → complex
+;   x : integer
+
+;35 -Funcion que determina si hay un numero entero 
+; (integer? x) → boolean?
+;   x : any/c
+
+;36 -Formula que determina el mínimo común múltiplo de dos enteros (exacto o inexacto).
+; (lcm x y ...) → integer
+;   x : integer
+;   y : integer
+
+;37 -Funcion que calcula el logaritmo en base e
+; (log x) → number
+;   x : number
+
+;38 - Funcion que determina la magnitud de un numero complejo 
+;(magnitude x) → real
+ ; x : number
+
+;39 - Funcion que crea un complejo a partir de una magnitud y un ángulo.
+;  (make-polar x y) → number
+;   x : real
+;   y : real
+
+;40 -Funcion crea un complejo a partir de una parte real y otra imaginaria.
+;(make-rectangular x y) → number
+ ; x : real
+ ; y : real
+
+ ;41 -Funcion que determina el numero mas grande
+;  (max x y ...) → real
+;   x : real
+;   y : real
+
+;42 - Funcion que determina el numero mas pequeño
+; (min x y ...) → real
+;   x : real
+;   y : real
+
+;43 -funcion que determina el residuo
+; (modulo x y) → integer
+;   x : integer
+;   y : integer
+
+;44 -Funcion que detemina si un numero es menor que 0
+; (negative? x) → boolean?
+;   x : real
+
+;45 -Funcion que convierte un numero en un string
+; (number->string x) → string
+;   x : number
+
+;46 - funcion que convierte un numero a un string con un numero especifico de digitos
+; (number->string-digits x p) → string
+;   x : number
+;   p : posint
+
+;47 -Funcion que determina si cualquier valor es un numero
+;(number? n) → boolean?
+;  n : any/c
 
 
