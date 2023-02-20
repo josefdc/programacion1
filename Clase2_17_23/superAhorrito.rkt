e8f3d23f30b22baaba74a1912987e56501485b72
Jose Duarte <josepipe0909@gmail.com>
2023-02-20 06:29:46 -0500

    cambios

---
+++ Clase2_17_23/superAhorrito.rkt
@@ -0,0 +1,21 @@
+#lang racket
+(displayln "Ingresa la cantidad de monedas de $20:")
+(define num-monedas-20 (read))
+  
+(displayln "Ingresa la cantidad de monedas de $50:")
+(define num-monedas-50 (read))
+  
+(displayln "Ingresa la cantidad de monedas de $100:")
+(define num-monedas-100 (read))
+  
+(displayln "Ingresa la cantidad de monedas de $200:")
+(define num-monedas-200 (read))
+  
+(displayln "Ingresa la cantidad de monedas de $500:")
+(define num-monedas-500 (read))
+  
+  (+ (* num-monedas-20 20)
+     (* num-monedas-50 50)
+     (* num-monedas-100 100)
+     (* num-monedas-200 200)
+     (* num-monedas-500 500))
