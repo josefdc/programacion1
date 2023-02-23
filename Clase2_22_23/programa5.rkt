eeae13384dcc9548a0bc42296c70e7bc1f631787
Jose Duarte <josepipe0909@gmail.com>
2023-02-23 09:53:29 -0500

    Programa -5, Calculo de costo de vajillas

---
+++ Clase2_22_23/programa5.rkt
@@ -0,0 +1,8 @@
+; Definimos la variable vajillas
+(define vajillas 22)
+
+; Calculamos el costo total utilizando la expresión
+(define costo-total (+ 3000 (* 50 vajillas)))
+
+; Mostramos el resultado por pantalla
+(displayln costo-total) ; 3700
