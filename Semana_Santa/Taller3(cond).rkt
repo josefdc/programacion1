aa913fbea5d45e8bbef4caff89434b561c27ace0
Jose Duarte <josepipe0909@gmail.com>
2023-04-04 09:01:58 -0500

    dia 2

--- Semana_Santa/Taller3(cond).rkt
+++ Semana_Santa/Taller3(cond).rkt
@@ -70,3 +70,134 @@
 
 (principal)
 
+;------3------
+
+; En un supermercado, se realizan descuentos por las compras a partir de unas bolitas de colores:
+; Verde 20%, amarilla 25%, negra del 30%; la blanca no aplica ningún descuento. Leer el importe 
+; de la compra y el color de la bolita e imprimir lo que debe pagar dicho cliente.
+
+
+; Función para aplicar el descuento según el color de la bolita
+(define (aplicar-descuento importe color)
+  (cond
+    [(equal? color 'verde) (* importe 0.8)]
+    [(equal? color 'amarilla) (* importe 0.75)]
+    [(equal? color 'negra) (* importe 0.7)]
+    [(equal? color 'blanca) importe]
+    [else (error "Color desconocido")]))
+
+
+; Función principal
+(define (calcular-pago)
+  (display "Ingrese el importe de la compra: ")
+  (define importe (read))
+  (display "Ingrese el color de la bolita (verde, amarilla, negra, blanca): ")
+  (define color (read))
+  (display "El cliente debe pagar: ")
+  (displayln (aplicar-descuento importe color)))
+
+; Ejemplo de uso
+(calcular-pago)
+
+;------5------
+; Leer una letra mayuscula e imprimir la letra  y si esta es recta (A,E,F,H,I,K,L,M,N,O,T,V,W,X,Y,Z), 
+; es curva (C,O,S,U,Q), o curva y recta (B,D,G,J,P,R)
+
+(define (leer-letra)
+  (read))
+
+; Función para determinar si una letra es recta
+(define (char-recta letra)
+      (or (char=? letra #\a) (char=? letra #\A)
+          (char=? letra #\e) (char=? letra #\E)
+          (char=? letra #\f) (char=? letra #\F)
+          (char=? letra #\h) (char=? letra #\H)
+          (char=? letra #\i) (char=? letra #\I)
+          (char=? letra #\k) (char=? letra #\K)
+          (char=? letra #\l) (char=? letra #\L)
+          (char=? letra #\m) (char=? letra #\M)
+          (char=? letra #\n) (char=? letra #\N)
+          (char=? letra #\o) (char=? letra #\O)
+          (char=? letra #\t) (char=? letra #\T)
+          (char=? letra #\v) (char=? letra #\V)
+          (char=? letra #\w) (char=? letra #\W)
+          (char=? letra #\x) (char=? letra #\X)
+          (char=? letra #\y) (char=? letra #\Y)
+          (char=? letra #\z) (char=? letra #\Z)))
+
+; Función para determinar si una letra es curva
+
+(define (char-curva letra)
+      (or (char=? letra #\c) (char=? letra #\C)
+          (char=? letra #\o) (char=? letra #\O)
+          (char=? letra #\s) (char=? letra #\S)
+          (char=? letra #\u) (char=? letra #\U)
+          (char=? letra #\q) (char=? letra #\Q)))
+; Función para determinar si una letra es curva y recta
+(define (char-curva-recta letra)
+      (or (char=? letra #\b) (char=? letra #\B)
+          (char=? letra #\d) (char=? letra #\D)
+          (char=? letra #\g) (char=? letra #\G)
+          (char=? letra #\j) (char=? letra #\J)
+          (char=? letra #\p) (char=? letra #\P)
+          (char=? letra #\r) (char=? letra #\R)))
+
+; Función principal
+(define (principal)
+  (display "Ingrese una letra: ")
+  (define letra (leer-letra))
+  (display "La letra ingresada es: ")
+  (display letra)
+  (newline)
+  (display "La letra ingresada es: ")
+  (cond
+    [(char-recta letra) (display "recta")]
+    [(char-curva letra) (display "curva")]
+    [(char-curva-recta letra) (display "curva y recta")]
+    [else (display "desconocida")])
+  (newline))
+
+
+;------6------
+; Elaborar un menu que calcule tres conversiones de tiempo
+; 1. De segundos a minutos
+; 2. De minutos a horas
+; 3. De horas a dias
+
+; Función para convertir segundos a minutos
+(define (segundos-minutos segundos)
+  (/ segundos 60))
+
+; Función para convertir minutos a horas
+(define (minutos-horas minutos)
+  (/ minutos 60))
+
+; Función para convertir horas a días
+(define (horas-dias horas)
+  (/ horas 24))
+
+; Función principal
+(define (principal)
+  (display "Ingrese una opción: ")
+  (displayln "1. De segundos a minutos")
+  (displayln "2. De minutos a horas")
+  (displayln "3. De horas a días")
+  (define opcion (read))
+  (cond
+    [(= opcion 1)
+     (display "Ingrese la cantidad de segundos: ")
+     (define segundos (read))
+     (display "La cantidad de minutos es: ")
+     (displayln (segundos-minutos segundos))]
+    [(= opcion 2)
+     (display "Ingrese la cantidad de minutos: ")
+     (define minutos (read))
+     (display "La cantidad de horas es: ")
+     (displayln (minutos-horas minutos))]
+    [(= opcion 3)
+     (display "Ingrese la cantidad de horas: ")
+     (define horas (read))
+     (display "La cantidad de días es: ")
+     (displayln (horas-dias horas))]
+    [else (error "Opción desconocida")]))
+
